2594e9904da82c5e46b3812bb1e60455f3b8a9c2
bevan222 <56305145+bevan222@users.noreply.github.com>
2022-04-03 17:26:17 +0800

    version1 complete

--- changlun_copy.py
+++ changlun_copy.py
@@ -3,6 +3,7 @@ import os
 import time
 import math
 from tkinter import BOTH
+from turtle import position
 from binance.client import Client
 from datetime import datetime
 from dotenv import load_dotenv
@@ -10,18 +11,21 @@ from binance.futures import Futures
 
 load_dotenv()
 
+
 class volume_robot:
     def __init__(self,ticker,api_key,api_secret,leverage,quantity,risk_time,add_percentage):
         self.ticker = ticker
         self.client = Client(api_key, api_secret)
         self.client_future = Futures(key=api_key, secret=api_secret)
         self.leverage = leverage
+        self.client.futures_change_leverage(symbol=self.ticker,leverage=self.leverage)
         self.quantity = quantity
         self.risk_time = int(risk_time)
-        self.precision = len(str(Decimal(self.client.get_symbol_ticker(symbol=self.ticker)['price']).normalize()).split('.')[1])
+        self.flatten_count = 0
+        self.price_precision = len(str(Decimal(self.client.get_symbol_ticker(symbol=self.ticker)['price']).normalize()).split('.')[1])
         self.up_order = {}
-        self.down_order = {}
-        self.first_open_quantity = round((float(self.quantity) / float(self.client.get_symbol_ticker(symbol=self.ticker)['price'])))
+        self.down_order = {} 
+        self.first_open_quantity = round((float(self.quantity) / float(self.client.get_symbol_ticker(symbol=self.ticker)['price'])),coin_presicion)
         self.add_percentage = add_percentage
     def position_amount(self):
         position = self.client_future.get_position_risk(symbol=self.ticker)[0]
@@ -43,22 +47,27 @@ class volume_robot:
         return False
 
     def create_order(self):
+        print("order start")
+        print( self.first_open_quantity)
         #print(self.risk_time)
         #print(self.quantity * math.pow(2,self.risk_time)-self.quantity*1)
         #print("max : " + str(self.first_open_quantity * math.pow(2,self.risk_time)-self.first_open_quantity*1))
-        if abs(self.position_amount()) < self.first_open_quantity * math.pow(2,self.risk_time)-self.first_open_quantity*1:
+        if self.flatten_count < self.risk_time:
+            print("flatten_count: " + str(self.flatten_count))
+            print("order mode 1")
             if self.position_amount() == 0:
                 self.change_leverage(10)
-                price = float(self.client.get_symbol_ticker(symbol=self.ticker)['price'])
-                price_up = round(price * (1+float(self.add_percentage)),self.precision)
-                price_down = round(price * (1-float(self.add_percentage)),self.precision)
+                
+                price = float(self.client.futures_mark_price(symbol=self.ticker,pair=self.ticker)['markPrice'])
+                price_up = round(price * (1+float(self.add_percentage)),self.price_precision)
+                price_down = round(price * (1-float(self.add_percentage)),self.price_precision)
                 print("price up:" + str(price_up))
                 print("price down:" + str(price_down))
                 self.down_order = self.client.futures_create_order(
                     symbol=self.ticker,
                     type='LIMIT',
                     side='BUY',  # Direction ('BUY' / 'SELL'), string
-                    quantity= round((float(self.quantity) / float(self.client.get_symbol_ticker(symbol=self.ticker)['price']))),  # Number of coins you wish to buy / sell, float
+                    quantity= self.first_open_quantity,  # Number of coins you wish to buy / sell, float
                     timeInForce = 'GTC',
                     price = price_down
                 )['orderId']
@@ -67,25 +76,31 @@ class volume_robot:
                     symbol=self.ticker,
                     type='LIMIT',
                     side='SELL',  # Direction ('BUY' / 'SELL'), string
-                    quantity= round((float(self.quantity) / float(self.client.get_symbol_ticker(symbol=self.ticker)['price']))),  # Number of coins you wish to buy / sell, float
+                    quantity= self.first_open_quantity,  # Number of coins you wish to buy / sell, float
                     timeInForce = 'GTC',
                     price = price_up
                 )['orderId']
             elif self.position_amount() > 0:
+                
+                self.flatten_count += 1 
+                if(self.flatten_count >=2):
+                    if self.client.futures_account_trades()[-1]['side'] != self.client.futures_account_trades()[-2]['side']:
+                        self.flatten_count = 1
+                print("flatten_count: " + str(self.flatten_count))
+                print("order mode 2")
                 print(self.position_amount())
-                price = float(self.client.futures_account_trades()[-1]['price'])
-                price_up = round(price * (1+float(self.add_percentage)),self.precision)
-                price_down = round(price * (1-float(self.add_percentage)),self.precision)
+                price = float(self.client.futures_account_trades(symbol=self.ticker)[-1]['price'])
+                price_up = round(price * (1+float(self.add_percentage)),self.price_precision)
+                price_down = round(price * (1-float(self.add_percentage)),self.price_precision)
                 print("price up:" + str(price_up))
                 print("price down:" + str(price_down))
-                pos_amount = abs(self.position_amount())
                 self.client_future.cancel_open_orders(symbol=self.ticker)
 
                 self.down_order = self.client.futures_create_order(
                     symbol=self.ticker,
                     type='LIMIT',
                     side='BUY',  # Direction ('BUY' / 'SELL'), string
-                    quantity=pos_amount*2,  # Number of coins you wish to buy / sell, float
+                    quantity=self.first_open_quantity * pow(2,self.flatten_count),  # Number of coins you wish to buy / sell, float
                     timeInForce = 'GTC',
                     price = price_down
                 )['orderId']
@@ -94,25 +109,31 @@ class volume_robot:
                     symbol=self.ticker,
                     type='LIMIT',
                     side='SELL',  # Direction ('BUY' / 'SELL'), string
-                    quantity=pos_amount*2,  # Number of coins you wish to buy / sell, float
+                    quantity=self.first_open_quantity * pow(2,self.flatten_count),  # Number of coins you wish to buy / sell, float
                     timeInForce = 'GTC',
                     price = price_up
                 )['orderId']
             elif self.position_amount() < 0:
+                print("flatten_count: " + str(self.flatten_count))
+                print("order mode 3")
+                self.flatten_count += 1 
+                if(self.flatten_count >=2):
+                    if self.client.futures_account_trades()[-1]['side'] != self.client.futures_account_trades()[-2]['side']:
+                        self.flatten_count = 1
+
                 print(self.position_amount())
-                price = float(self.client.futures_account_trades()[-1]['price'])
-                price_up = round(price * (1+float(self.add_percentage)),self.precision)
-                price_down = round(price * (1-float(self.add_percentage)),self.precision)
+                price = float(self.client.futures_account_trades(symbol=self.ticker)[-1]['price'])
+                price_up = round(price * (1+float(self.add_percentage)),self.price_precision)
+                price_down = round(price * (1-float(self.add_percentage)),self.price_precision)
                 print("price up:" + str(price_up))
                 print("price down:" + str(price_down))
-                pos_amount = abs(float(robot.client.futures_account_trades(symbol=robot.ticker)[-1]['qty']))
                 self.client_future.cancel_open_orders(symbol=self.ticker)
 
                 self.up_order = self.client.futures_create_order(
                     symbol=self.ticker,
                     type='LIMIT',
                     side='SELL',  # Direction ('BUY' / 'SELL'), string
-                    quantity=pos_amount*2,  # Number of coins you wish to buy / sell, float
+                    quantity=self.first_open_quantity * pow(2,self.flatten_count),  # Number of coins you wish to buy / sell, float
                     timeInForce = 'GTC',
                     price = price_up
                 )['orderId']
@@ -121,7 +142,7 @@ class volume_robot:
                     symbol=self.ticker,
                     type='LIMIT',
                     side='BUY',  # Direction ('BUY' / 'SELL'), string
-                    quantity=pos_amount*2,  # Number of coins you wish to buy / sell, float
+                    quantity=self.first_open_quantity * pow(2,self.flatten_count),  # Number of coins you wish to buy / sell, float
                     timeInForce = 'GTC',
                     price = price_down
                 )['orderId']
@@ -135,14 +156,15 @@ quantity = float(os.getenv("USDTQUANTITY"))
 ticker = os.getenv("TICKER")
 risk_time = os.getenv("RISK_TIME")
 add_percentage = os.getenv("ADD_PERCENTAGE")
-
+coin_presicion = int(os.getenv("COIN_PRECISION"))
+print(api_key)
+print(api_secret)
 
 robot  = volume_robot(ticker,api_key,api_secret,leverage,quantity,risk_time,add_percentage)
 print("leverage: " + str(robot.leverage))
 print(robot.client.futures_position_information(symbol=robot.ticker))
 print("position: " + str(robot.position_amount()))
 robot.create_order()
-
 
 
 while True:
@@ -155,11 +177,10 @@ while True:
     down =robot.client.futures_get_order(symbol=robot.ticker,orderId=robot.down_order)
     print("up_order inform: " +up['status'] +" "+ up['side'] + " " + up['price'])
     print("down_order inform: " +down['status']+" "+ down['side'] + " " + down['price'])
+    print(robot.client.futures_account_trades()[-1])
     if robot.check_order_deal():
         time.sleep(5)
         robot.create_order()
         time.sleep(3)
     print()
-    #print(robot.client.futures_account_trades(symbol=robot.ticker)[-1])
-    #print(robot.client.futures_get_order(symbol=robot.ticker,orderId=robot.client.futures_account_trades(symbol=robot.ticker)[-1]['orderId']))
     time.sleep(5)
